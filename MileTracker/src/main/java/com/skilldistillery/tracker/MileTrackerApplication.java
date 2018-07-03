bced6ed7dc7caeb18376adc3b13f063290cb0de6
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-07-03 16:15:26 -0600

    initial commit

---
+++ MileTracker/src/main/java/com/skilldistillery/tracker/MileTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MileTrackerApplication.class, args);
+	}
+}
